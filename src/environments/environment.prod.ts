67ab3fc75e53991888eb3c964a898287e3ce6eff
helengiraldo <helen8335@gmail.com>
2026-04-02 16:39:05 -0500

    fix: add environment files for build

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.pawsoft.online',
+  cloudinary: {
+    cloudName: 'dp24p2jhq',
+    uploadPreset: 'pawsoft_pets'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: `http://${window.location.hostname}:8080`,
+  cloudinary: {
+    cloudName: 'dp24p2jhq',
+    uploadPreset: 'pawsoft_pets'
+  }
+};
